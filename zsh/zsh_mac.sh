144feddaf605d3cad98558f8ea7c9cf367b2f1e1
Niclas <niclas2102@gmail.com>
2019-05-21 22:08:00 +0200

    Splitted zshrc up for mac and linux

---
+++ zsh/zsh_mac.sh
@@ -0,0 +1,19 @@
+# mounts
+_mount () {
+    diskutil umount force /Users/niclaseich/mounts/$1;
+    sshfs -o reconnect -o defer_permissions -o negative_vncache -o volname=$1 $2 /Users/niclaseich/mounts/$1;
+}
+
+## vispa
+alias m_vispa_NiclasEich='_mount vispa/home/NiclasEich/ NEvispa-portal:'
+alias m_vispa_NiclasEich_scratch='_mount vispa/scratch/NiclasEich/ NEvispa-portal:/net/scratch/NiclasEich'
+
+alias ls='ls -G'
+export CLICOLOR=1
+export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
+
+# Virtualenvwrapper things
+#
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+source /usr/local/bin/virtualenvwrapper.sh
