68fb910e59d122d07588773438c544ba546f7d33
Niclas Eich <niclas2102@gmail.com>
2024-02-20 17:25:39 +0100

    Color on startup working

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -2,8 +2,8 @@ function ColorSetup(color)
 	color = color or "kanagawa-wave"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", {bg ="none"})
-	vim.api.nvim_set_hl(0, "NormalFloat", {bg ="none"})
+	-- vim.api.nvim_set_hl(0, "Normal", {bg ="none"})
+	-- vim.api.nvim_set_hl(0, "NormalFloat", {bg ="none"})
 end
 
-ColorSetup()
+ColorSetup("kanagawa-wave")
--- nvim/lua/icecream24/colorscheme.lua
+++ nvim/lua/icecream24/colorscheme.lua
@@ -25,4 +25,4 @@ require('kanagawa').setup({
 })
 
 -- setup must be called before loading
-vim.cmd("colorscheme kanagawa-wave")
+vim.cmd.colorscheme('kanagawa-wave')
