377beaeeaaf4827d26e2e2273cae36721687cda9
Niclas Eich <niclas2102@gmail.com>
2025-08-04 12:15:40 +0200

    add deletion possibility in harpoon picker through telescope UI

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -37,6 +37,46 @@ local function toggle_telescope(harpoon_files)
     }):find()
 end
 
+
+-- add deletion functionality 
+
+local function toggle_telescope(harpoon_files)
+    local finder = function()
+        local paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+            table.insert(paths, item.value)
+        end
+
+        return require("telescope.finders").new_table({
+            results = paths,
+        })
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = finder(),
+        previewer = true,
+        sorter = require("telescope.config").values.generic_sorter({}),
+        layout_config = {
+            height = 0.4,
+            width = 0.5,
+            prompt_position = "top",
+            preview_cutoff = 120,
+        },
+        attach_mappings = function(prompt_bufnr, map)
+            map("i", "<C-d>", function()
+                local state = require("telescope.actions.state")
+                local selected_entry = state.get_selected_entry()
+                local current_picker = state.get_current_picker(prompt_bufnr)
+
+                table.remove(harpoon_files.items, selected_entry.index)
+                current_picker:refresh(finder())
+            end)
+            return true
+        end,
+    }):find()
+end
+
+
 vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
     { desc = "Open harpoon window" })
-
